44fbc97c5c5b1a8326223c8049f1f021807c571d
heineiuo <heineiuo@gmail.com>
2020-06-23 14:20:34 +0800

    v0.0.11

--- docker/splashdb-node/entrypoint.js
+++ docker/splashdb-node/entrypoint.js
@@ -3,7 +3,7 @@ const cp = require('child_process')
 const NODE_PROF = process.env.NODE_PROF
 
 if (NODE_PROF === 'true') {
-  cp.execSync(`node index.js --prof`)
+  cp.execSync(`node --prof index.js`)
 } else {
   cp.execSync(`node index.js`)
 }

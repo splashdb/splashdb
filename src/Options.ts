90a66388d5ee94cf34563182e4253efbf6283084
heineiuo <heineiuo@gmail.com>
2020-08-06 18:36:52 +0800

    Update

--- src/Options.ts
+++
@@ -1,9 +0,0 @@
-export type SplashDBServerOptions = {
-  debug?: boolean
-  secure?: boolean
-  secureKey?: string | Buffer
-  secureCert?: string | Buffer
-  adminPassword?: string
-  port?: number
-  dbpath?: string
-}

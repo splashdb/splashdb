fbdf555d9e8dae9300707a42c05c99b67b23b46a
heineiuo <heineiuo@gmail.com>
2020-08-07 13:13:24 +0800

    Remove legacy files

--- packages/ripplesql/fixtures/random.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto'
-
-export function random(keySize = 16, valueSize = 64): [string, string] {
-  const key = crypto.randomBytes(keySize).toString('hex')
-  const value = crypto.randomBytes(valueSize).toString('hex')
-  return [key, value]
-}

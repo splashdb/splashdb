dbeef69b5c8e0980b058f3429a04e35ad35d0298
heineiuo <heineiuo@gmail.com>
2020-08-14 10:28:56 +0800

    fix: run publish-docker failed

--- packages/mongo/publish-docker.sh
+++ packages/mongo/publish-docker.sh
@@ -2,5 +2,5 @@
 
 export IMAGE_VERSION=$(node -p "require('./package.json').version")
 echo $IMAGE_VERSION
-docker build -t splashdb/mongo-node:${IMAGE_VERSION}
+docker build . -t splashdb/mongo-node:${IMAGE_VERSION}
 docker push splashdb/mongo-node:${IMAGE_VERSION}

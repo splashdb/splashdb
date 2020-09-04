b2273730ce4701ae8724a085cc2733d1ef00f858
heineiuo <heineiuo@gmail.com>
2020-09-04 11:50:09 +0800

    Rename files to meet spec

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // testEnvironment: 'jest-environment-uint8array',
+  // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  globalSetup: './fixtures/jest-setup.ts',
+  coverageDirectory: './coverage',
+  coverageReporters: ['lcov'],
+}
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  prettierPath: './node_modules/prettier',
+  parser: 'typescript',
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
